9577108c198b5b9332b273d7bad1791663aaa4bd
kurachat <103978716+kurachat@users.noreply.github.com>
2024-03-29 02:36:53 +0700

    booking

--- resources/views/booking/book_from.blade.php
+++ resources/views/booking/book_from.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.adminLayout')
+@extends('layout.' . session('role_name'))
 @section('title','from')
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/datepick.css') }}">
